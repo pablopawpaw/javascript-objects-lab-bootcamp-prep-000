0d1d3862400040fec5925612a0d222dcefdf5054
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-04-17 19:16:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,23 @@ var recipes = {}
 
 
 function updateObjectWithKeyAndValue(object, key, value) {
+<<<<<<< HEAD
   var newObj = Object.assign({},object)
   newObj[key] = value
   return newObj
 }
 
+=======
+  var newObj = object[key] = value
+  return newObj
+}
+
+updateObjectWithKeyAndValue(recipes,'diary','milk')
+console.log(recipes)
+updateObjectWithKeyAndValue(recipes,'diary','cheese')
+console.log(recipes)
+
+>>>>>>> c32f3d0f1ee91eaf21b67245e42d5fcce185673f
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
@@ -16,7 +28,11 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({},object)
   delete newObj[key]
+<<<<<<< HEAD
   return newObj
+=======
+  return object
+>>>>>>> c32f3d0f1ee91eaf21b67245e42d5fcce185673f
 }
 
 
